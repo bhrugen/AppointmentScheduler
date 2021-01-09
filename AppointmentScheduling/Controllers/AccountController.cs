27d69493d2e133cd3b4c2e1a3317bf0c0db43d36
Bhrugen Patel <bhrugen1990@gmail.com>
2021-01-09 15:18:08 -0600

    Section 2 - Login View Model

---
+++ AppointmentScheduling/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using AppointmentScheduling.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
