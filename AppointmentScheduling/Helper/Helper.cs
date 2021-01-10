7f5f9e284494c8263bd2d30e0c2fdd1de6476ba0
Bhrugen Patel <bhrugen1990@gmail.com>
2021-01-09 19:11:59 -0600

    Section 2 - Helper Class

---
+++ AppointmentScheduling/Helper/Helper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Helper
+{
+    public static class Helper
+    {
+        public static string Admin = "Admin";
+        public static string Patient = "Patient";
+        public static string Doctor = "Doctor";
+
+        public static List<SelectListItem> GetRolesForDropDown()
+        {
+            return new List<SelectListItem>
+            {
+                new SelectListItem{Value=Helper.Admin,Text=Helper.Admin},
+                new SelectListItem{Value=Helper.Patient,Text=Helper.Patient},
+                new SelectListItem{Value=Helper.Doctor,Text=Helper.Doctor}
+            };
+        }
+    }
+}
