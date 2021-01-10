e6b3c71fec1903b2601cab0947f8cc276b6d8376
Bhrugen Patel <bhrugen1990@gmail.com>
2021-01-10 14:37:42 -0600

    Section 3 - Appointment Setup

---
+++ AppointmentScheduling/Models/ViewModels/DoctorVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Models.ViewModels
+{
+    public class DoctorVM
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ AppointmentScheduling/Services/AppointmentService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Services
+{
+    public class AppointmentService : IAppointmentService
+    {
+    }
+}
