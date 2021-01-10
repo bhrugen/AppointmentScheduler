4194cbb160c1e7ee543da2f40af1105374d3a564
Bhrugen Patel <bhrugen1990@gmail.com>
2021-01-09 22:17:08 -0600

    Section 2 - Sign In Navbar

--- AppointmentScheduling/Startup.cs
+++ AppointmentScheduling/Startup.cs
@@ -51,7 +51,7 @@ namespace AppointmentScheduling
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
