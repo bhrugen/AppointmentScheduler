d4d940ec543e2b5ac2379ca4c7d6a600995f0f47
Bhrugen Patel <bhrugen1990@gmail.com>
2021-01-10 17:50:35 -0600

    Section 3 - Show Calendar

--- AppointmentScheduling/wwwroot/js/script-custom-calendar.js
+++ AppointmentScheduling/wwwroot/js/script-custom-calendar.js
@@ -1,1 +1,24 @@
-﻿
+﻿$(document).ready(function () {
+    InitializeCalendar();
+});
+
+function InitializeCalendar() {
+    try {
+        $('#calendar').fullCalendar({
+            timezone: false,
+            header: {
+                left: 'prev,next,today',
+                center: 'title',
+                right: 'month,agendaWeek,agendaDay'
+            },
+            selectable: true,
+            editable: false
+
+
+        });
+    }
+    catch (e) {
+        alert(e);
+    }
+
+}
