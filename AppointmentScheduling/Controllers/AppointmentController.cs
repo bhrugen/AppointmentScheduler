0eda20f05579382c11cc119da71a2618deb37eef
Bhrugen Patel <bhrugen1990@gmail.com>
2021-01-10 15:33:26 -0600

    Section 3 - Display Doctor DropDown

--- AppointmentScheduling/Controllers/AppointmentController.cs
+++ AppointmentScheduling/Controllers/AppointmentController.cs
@@ -19,7 +19,7 @@ namespace AppointmentScheduling.Controllers
 
         public IActionResult Index()
         {
-            _appointmentService.GetDoctorList();
+            ViewBag.DoctorList = _appointmentService.GetDoctorList();
             return View();
         }
     }
