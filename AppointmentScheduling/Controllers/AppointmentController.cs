65c679e77e04c80043697b213b512b37532f460d
Bhrugen Patel <bhrugen1990@gmail.com>
2021-01-10 18:35:56 -0600

    Section 3 - Modal for Add_Edit Appointment

--- AppointmentScheduling/Controllers/AppointmentController.cs
+++ AppointmentScheduling/Controllers/AppointmentController.cs
@@ -19,7 +19,10 @@ namespace AppointmentScheduling.Controllers
 
         public IActionResult Index()
         {
+
             ViewBag.DoctorList = _appointmentService.GetDoctorList();
+            ViewBag.PatientList = _appointmentService.GetPatientList();
+
             return View();
         }
     }
