34ae7c8d9482ee78efa804a178ace205a9e0f0fb
Bhrugen Patel <bhrugen1990@gmail.com>
2021-01-17 16:59:35 -0600

    Section 3 - Validations

--- AppointmentScheduling/wwwroot/js/script-custom-calendar.js
+++ AppointmentScheduling/wwwroot/js/script-custom-calendar.js
@@ -48,32 +48,56 @@ function onCloseModal() {
 }
 
 function onSubmitForm() {
-    var requestData = {
-        Id: parseInt($("#id").val()),
-        Title: $("#title").val(),
-        Description: $("#description").val(),
-        StartDate: $("#appointmentDate").val(),
-        Duriation: $("#duration").val(),
-        DoctorId: $("#doctorId").val(),
-        PatientId: $("#patientId").val(),
-    };
+    if (checkValidation()) {
+        var requestData = {
+            Id: parseInt($("#id").val()),
+            Title: $("#title").val(),
+            Description: $("#description").val(),
+            StartDate: $("#appointmentDate").val(),
+            Duriation: $("#duration").val(),
+            DoctorId: $("#doctorId").val(),
+            PatientId: $("#patientId").val(),
+        };
 
-    $.ajax({
-        url: routeURL + '/api/Appointment/SaveCalendarData',
-        type: 'POST',
-        data: JSON.stringify(requestData),
-        contentType: 'application/json',
-        success: function (response) {
-            if (response.status === 1 || response.status === 2) {
-                $.notify(response.message, "success");
-                onCloseModal();
+        $.ajax({
+            url: routeURL + '/api/Appointment/SaveCalendarData',
+            type: 'POST',
+            data: JSON.stringify(requestData),
+            contentType: 'application/json',
+            success: function (response) {
+                if (response.status === 1 || response.status === 2) {
+                    $.notify(response.message, "success");
+                    onCloseModal();
+                }
+                else {
+                    $.notify(response.message, "error");
+                }
+            },
+            error: function (xhr) {
+                $.notify("Error", "error");
             }
-            else {
-                $.notify(response.message, "error");
-            }
-        },
-        error: function (xhr) {
-            $.notify("Error", "error");
-        }
-    });
+        });
+    }
+}
+
+function checkValidation() {
+    var isValid = true;
+    if ($("#title").val() === undefined || $("#title").val() === "") {
+        isValid = false;
+        $("#title").addClass('error');
+    }
+    else {
+        $("#title").removeClass('error');
+    }
+
+    if ($("#appointmentDate").val() === undefined || $("#appointmentDate").val() === "") {
+        isValid = false;
+        $("#appointmentDate").addClass('error');
+    }
+    else {
+        $("#appointmentDate").removeClass('error');
+    }
+
+    return isValid;
+
 }
