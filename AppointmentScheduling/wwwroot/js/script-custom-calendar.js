c8c41e89528aca01a0ac778b6979099ea538b627
Bhrugen Patel <bhrugen1990@gmail.com>
2021-01-17 11:34:31 -0600

    Section 3 - Update Full Calendar

--- AppointmentScheduling/wwwroot/js/script-custom-calendar.js
+++ AppointmentScheduling/wwwroot/js/script-custom-calendar.js
@@ -4,21 +4,25 @@
 
 function InitializeCalendar() {
     try {
-        $('#calendar').fullCalendar({
-            timezone: false,
-            header: {
-                left: 'prev,next,today',
-                center: 'title',
-                right: 'month,agendaWeek,agendaDay'
-            },
-            selectable: true,
-            editable: false,
-            select: function (event) {
-                onShowModal(event, null);
-            }
 
+        
+            var calendarEl = document.getElementById('calendar');
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'dayGridMonth',
+                headerToolbar: {
+                    left: 'prev,next,today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                },
+                selectable: true,
+                editable: false,
+                select: function (event) {
+                    onShowModal(event, null);
+                }
+            });
+            calendar.render();
+       
 
-        });
     }
     catch (e) {
         alert(e);
