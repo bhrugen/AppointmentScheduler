a873decee2e1d33d90ba43e22b6e469712d9f885
Bhrugen Patel <bhrugen1990@gmail.com>
2021-01-17 20:02:18 -0600

    Section 4 - Display Patient Name and Status

--- AppointmentScheduling/wwwroot/js/script-custom-calendar.js
+++ AppointmentScheduling/wwwroot/js/script-custom-calendar.js
@@ -80,7 +80,14 @@ function onShowModal(obj, isEventDetail) {
         $("#doctorId").val(obj.doctorId);
         $("#patientId").val(obj.patientId);
         $("#id").val(obj.id);
-
+        $("#lblPatientName").html(obj.patientName);
+        $("#lblDoctorName").html(obj.doctorName);
+        if (obj.isDoctorApproved) {
+            $("#lblStatus").html('Approved');
+        }
+        else {
+            $("#lblStatus").html('Pending');
+        }
     }
     else {
         $("#appointmentDate").val(obj.startStr + " " + new moment().format("hh:mm A"));
